77ac464dee9855cdfef585787940868812e0daa5
Dmitry <wot43ak@mail.ru>
2023-11-09 22:35:44 +0300

    v2

--- src/main/java/net/proselyte/springbootdemo/controller/UserController.java
+++ src/main/java/net/proselyte/springbootdemo/controller/UserController.java
@@ -11,22 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.script.ScriptContext;
-import java.io.Console;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class UserController {
-    private List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {{
-        add(new HashMap<String, String>() {{ put("id", "1"); put("text", "First message"); }});
-        add(new HashMap<String, String>() {{ put("id", "2"); put("text", "Second message"); }});
-        add(new HashMap<String, String>() {{ put("id", "3"); put("text", "Third message"); }});
-    }};
+
     private UserService userService;
 
     public static int idd;
